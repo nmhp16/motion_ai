12cc92d05d11e5ec3b7d4d1bada45fdef79f9ff3
nmhp16 <nmhieu.pham@gmail.com>
2024-10-16 16:34:12 -0700

    Minor changes

--- src/main/java/com/instructor/algorithms/DecisionTree.java
+++ src/main/java/com/instructor/algorithms/DecisionTree.java
@@ -1,20 +1,9 @@
 package com.instructor.algorithms;
 
 // TODO: COMPLETE DECISION TREE FOR CLASSIFYING CORRECT/INCORRECT POSE
+/**
+ * Decision Tree class for classifying poses as correct or incorrect
+ */
 public class DecisionTree {
 
-    private class Node {
-        Node() {
-
-        }
-    }
-
-    private Node root;
-
-    /**
-     * Construct a decision tree based on features like angle, speed, and keypoints
-     */
-    public DecisionTree() {
-        root = new Node();
-    }
 }

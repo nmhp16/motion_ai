2da6fb96db0aa5f7f35d7174efd10405fac23a72
nmhp16 <nmhieu.pham@gmail.com>
2024-10-26 23:58:10 -0700

    Updated options for pose processing

---
+++ src/main/java/com/instructor/main/Main.java
@@ -0,0 +1,110 @@
+package com.instructor.main;
+
+import com.instructor.data.PoseDataProcessing;
+import com.instructor.data.PoseDataReader;
+import com.instructor.evaluation.PoseScoring;
+
+import java.util.Scanner;
+
+import com.instructor.data.*;
+import com.instructor.evaluation.PoseScoring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        // Instantiate PoseDataReader and PoseDataProcessing and Pose Scoring
+        PoseDataReader poseDataReader = new PoseDataReader();
+        PoseDataProcessing poseDataProcessing = new PoseDataProcessing();
+        PoseScoring poseScoring = new PoseScoring();
+
+        // Map <Keypoints (ex: left_leg), Map<Frame (ex: 1), coordinates (x,y,z)>>
+        // Instantiate Hashmap to store user key points
+        Map<String, Map<Integer, float[]>> userKeypointsMap = new HashMap<>();
+        Map<String, Map<Integer, float[]>> proKeypointsMap = new HashMap<>();
+
+        // Create the entry point for communication
+        Starter handler = new Starter();
+
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // User input file path
+        String userFile = null;
+
+        // User input options
+        System.out.println();
+        System.out.println("Please select options below:");
+        System.out.println("1: Record video.");
+        System.out.println("2: Upload video.");
+        System.out.println("0: Test.");
+
+        int option = scanner.nextInt();
+        scanner.nextLine(); // Skip
+
+        switch (option) {
+            case 1: // Capture user video
+                System.out.println("Starting video capture from Python...");
+                handler.runCapturePoseEstimation();
+
+                // Process user video to get data
+                userKeypointsMap = handler.processUserFile();
+
+                // Process user data
+                userKeypointsMap = handler.processPoseData(userKeypointsMap);
+
+                // TODO: Decision Tree for classification to get correct pro video
+
+                break;
+
+            case 2: // Get user uploaded video
+                System.out.println("Enter the path for the user keypoints file: ");
+                userFile = scanner.nextLine(); // Assign value to userFile
+
+                if (userFile != null) {
+                    // Allow user to upload video to be compared
+                    handler.runUploadPoseEstimation(userFile, "Beginner");
+                } else {
+                    System.out.println("No valid user file path provided");
+                }
+
+                // Process user video to get data
+                userKeypointsMap = handler.processUserFile();
+
+                // Process user data
+                userKeypointsMap = handler.processPoseData(userKeypointsMap);
+
+                // TODO: Decision Tree for classification to get correct pro video
+                break;
+
+            default: // Test
+                // TODO: Remove once done testing
+                // Load keypoints from files
+                userKeypointsMap = poseDataReader
+                        .readKeypointsFromFile("./motion_database/ballet_spin/beginner.txt");
+                proKeypointsMap = poseDataReader.readKeypointsFromFile("./motion_database/ballet_spin/pro.txt");
+
+                userKeypointsMap = handler.processPoseData(userKeypointsMap);
+                proKeypointsMap = handler.processPoseData(proKeypointsMap);
+
+                // Calculate similarity score between user and pro
+                float similarityScore = poseDataProcessing.calculateSimilarity(userKeypointsMap, proKeypointsMap);
+
+                // Assume max similarity
+                float maxSimilarity = 2.0f; // Replace with actual value
+
+                // Calculate final score
+                int finalScore = poseScoring.calculateScore(similarityScore, maxSimilarity);
+
+                // Output similarity score and final score
+                System.out.println();
+                System.out.println("Similarity Score: " + similarityScore);
+                System.out.println("Final Score (out of 100): " + finalScore);
+
+                break;
+        }
+
+        scanner.close();
+    }
+}
--- src/main/java/com/instructor/main/Starter.java
+++ src/main/java/com/instructor/main/Starter.java
@@ -1,116 +1,174 @@
 package com.instructor.main;
 
-import com.instructor.data.*;
-import com.instructor.evaluation.PoseScoring;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.instructor.data.PoseDataProcessing;
+import com.instructor.data.PoseDataReader;
+import com.instructor.evaluation.PoseScoring;
 
 public class Starter {
+        private PoseDataReader poseDataReader;
+        private PoseDataProcessing poseDataProcessing;
 
-    /**
-     * Method to run the Python script
-     */
-    private void runPythonScript() {
-        try {
-            // Define the command to run the Python script
-            String pythonScriptPath = "./pose_detection/PoseDetection.py"; // Relative path
-            ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
-
-            // Set the redirect error stream to true to capture errors
-            processBuilder.redirectErrorStream(true);
-
-            // Start the process
-            Process process = processBuilder.start();
-
-            // Read the output of the script
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            // Wait for the process to finish
-            int exitCode = process.waitFor();
-            System.out.println("Python script exited with code: " + exitCode);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        public Starter() {
+                // Initialize PoseDataReader
+                this.poseDataReader = new PoseDataReader();
+                this.poseDataProcessing = new PoseDataProcessing();
         }
-    }
 
-    /**
-     * Main
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
-        // Instantiate PoseDataReader and PoseDataProcessing and Pose Scoring
-        PoseDataReader poseDataReader = new PoseDataReader();
-        PoseDataProcessing poseDataProcessing = new PoseDataProcessing();
-        PoseScoring poseScoring = new PoseScoring();
+        /**
+         * Method to capture user video for pose estimation
+         */
+        public void runCapturePoseEstimation() {
+                try {
+                        // Define the command to run the Python script
+                        String pythonScriptPath = "./pose_detection/PoseDetection.py"; // Relative path
+                        ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath);
 
-        // Map <Keypoints (ex: left_leg), Map<Frame (ex: 1), coordinates (x,y,z)>>
-        // Instantiate Hashmap to store user key points
-        Map<String, Map<Integer, float[]>> userKeypointsMap = new HashMap<>();
-        Map<String, Map<Integer, float[]>> proKeypointsMap = new HashMap<>();
+                        // Set the redirect error stream to true to capture errors
+                        processBuilder.redirectErrorStream(true);
 
-        // Create the entry point for communication
-        Starter handler = new Starter();
+                        // Start the process
+                        Process process = processBuilder.start();
 
-        // TODO: Remove // Once done testing
-        // Start video to capture user dance, and generate a .txt file
-        // System.out.println("Starting video capture from Python...");
-        // handler.runPythonScript();
+                        // Read the output of the script
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        String line;
 
-        // TODO: Remove // Once done testing
-        // Read this .txt file to populate userKeypoints
-        // userKeypointsMap = poseDataReader.readKeypointsFromFile("userKeypoints.txt");
+                        while ((line = reader.readLine()) != null) {
+                                System.out.println(line);
+                        }
 
-        // Load keypoints from files
-        userKeypointsMap = poseDataReader
-                .readKeypointsFromFile("./motion_database/ballet_spin/beginner.txt");
-        proKeypointsMap = poseDataReader.readKeypointsFromFile("./motion_database/ballet_spin/pro.txt");
+                        // Wait for the process to finish
+                        int exitCode = process.waitFor();
+                        System.out.println("Python script exited with code: " + exitCode);
 
-        // Clean missing keypoints for both user and pro datasets
-        Map<String, Map<Integer, float[]>> cleanedUserKeypoints = poseDataProcessing
-                .cleanMissingKeypoints(userKeypointsMap);
-        Map<String, Map<Integer, float[]>> cleanedProKeypoints = poseDataProcessing
-                .cleanMissingKeypoints(proKeypointsMap);
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+        }
 
-        // Smooth the cleaned keypoints with defined window size
-        int windowSize = 5;
-        Map<String, Map<Integer, float[]>> smoothedUserKeypoints = poseDataProcessing
-                .smoothKeypoints(cleanedUserKeypoints, windowSize);
-        Map<String, Map<Integer, float[]>> smoothedProKeypoints = poseDataProcessing
-                .smoothKeypoints(cleanedProKeypoints, windowSize);
+        /**
+         * Method to upload video for pose estimation
+         * 
+         * @param videoPath Path to video
+         * @param videoType Type of video ("beginner" or "pro")
+         */
+        public void runUploadPoseEstimation(String videoPath, String videoType) {
+                List<String> command = new ArrayList<>();
+                command.add("python"); // Depending on setup
+                command.add("./pose_detection/PoseUpload.py"); // Path to PoseUpload.py
+                command.add("--video");
+                command.add(videoPath);
+                command.add("--type");
+                command.add(videoType);
 
-        // Normalize the cleaned keypoints for both user and pro
-        Map<String, Map<Integer, float[]>> normalizedUserKeypoints = poseDataProcessing
-                .normalizeKeypoints(smoothedUserKeypoints);
-        Map<String, Map<Integer, float[]>> normalizedProKeypoints = poseDataProcessing
-                .normalizeKeypoints(smoothedProKeypoints);
+                try {
+                        ProcessBuilder pb = new ProcessBuilder(command);
+                        pb.redirectErrorStream(true); // Merge error and output streams
+                        Process process = pb.start();
 
-        // Calculate similarity score between user and pro
-        float similarityScore = poseDataProcessing.calculateSimilarity(normalizedUserKeypoints, normalizedProKeypoints);
+                        // Read the output of the Python script
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                                System.out.println(line); // Print the Python output to Java console
+                        }
 
-        // Assume max similarity
-        float maxSimilarity = 2.0f; // Replace with actual value
+                        // Wait for the process to complete
+                        int exitCode = process.waitFor();
 
-        // Calculate final score
-        int finalScore = poseScoring.calculateScore(similarityScore, maxSimilarity);
+                        if (exitCode == 0) {
+                                System.out.println("Pose estimation completed successfully.");
+                        } else {
+                                System.out.println("Pose estimation encountered an error. Exit code: " + exitCode);
+                        }
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+        }
 
-        // Output similarity score and final score
-        System.out.println();
-        System.out.println("Similarity Score: " + similarityScore);
-        System.out.println("Final Score (out of 100): " + finalScore);
+        /**
+         * Method to get the most recent file created by user from file name list
+         * 
+         * @param filePath Location of txt file containing user generated file
+         * @return Most recent file name
+         */
+        public String readLastSavedFileName(String filePath) {
+                String lastLine = null;
 
-        // TODO: Calculate DTW Score
+                try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                        String currentLine;
 
-        // TODO: Decision Tree for classification
+                        while ((currentLine = reader.readLine()) != null) {
+                                lastLine = currentLine; // Keep updating lastLine until end of file
+                        }
+                } catch (IOException e) {
+                        System.err.println("Error reading the last saved file name: " + e.getMessage());
+                }
+                return lastLine;
+        }
 
-    }
+        /**
+         * Method to process the user file for pose estimation
+         * 
+         * @return Updated map of user keypoints
+         */
+        public Map<String, Map<Integer, float[]>> processUserFile() {
+                Map<String, Map<Integer, float[]>> userKeypointsMap = new HashMap<>(); // Initialize map to store user
+                                                                                       // data
+
+                // Read the last saved filename from last_saved_filename.txt
+                String keypointsFileName = readLastSavedFileName("last_saved_filename.txt");
+
+                if (keypointsFileName != null) {
+                        String filePath = "keypointsFilename";
+
+                        // Use the file name to read keypoints from the newly created file
+                        userKeypointsMap = poseDataReader.readKeypointsFromFile(filePath);
+
+                        // Process the keypoints map as needed
+                        System.out.println("Successfully read keypoints from: " + filePath);
+                } else {
+                        System.out.println("Failed to read the keypoints file name.");
+                }
+
+                return userKeypointsMap; // Return the populated map
+        }
+
+        /**
+         * Method to process pose data from given map
+         * 
+         * @param keypointsMap Map to be process
+         * @return Processed map
+         */
+        public Map<String, Map<Integer, float[]>> processPoseData(Map<String, Map<Integer, float[]>> keypointsMap) {
+                Map<String, Map<Integer, float[]>> processedMap; // Initialize keypoints map to store processed data
+
+                // Clean missing keypoints for both user and pro datasets
+                processedMap = poseDataProcessing
+                                .cleanMissingKeypoints(keypointsMap);
+
+                // Smooth the cleaned keypoints with defined window size
+                int windowSize = 5;
+
+                processedMap = poseDataProcessing
+                                .smoothKeypoints(processedMap, windowSize);
+
+                // Normalize the cleaned keypoints for both user and pro
+                processedMap = poseDataProcessing
+                                .normalizeKeypoints(processedMap);
+
+                // TODO: Calculate DTW Score
+
+                return processedMap;
+        }
 }
